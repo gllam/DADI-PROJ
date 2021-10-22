509b676b65a2c087e395367b6c6d5ec7eb3984c5
gllam <david.miranda.domingues@gmail.com>
2021-10-22 20:15:44 +0100

    More functions added to PuppetMaster

---
+++ DIDAOperatorSample/PuppetMaster/Utils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PuppetMaster
+{
+    class Utils
+    {
+        public static String ReadFromFile(String filePath)
+        {
+            return System.IO.File.ReadAllText(@filePath);
+        }
+
+    }
+}
