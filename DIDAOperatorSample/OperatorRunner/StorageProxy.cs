91b0e4b3aa851d9ccaddcd633bb13399ea430e34
bcv20 <64037304+bcv20@users.noreply.github.com>
2021-11-01 22:21:35 +0000

    locate function implemented

--- DIDAOperatorSample/OperatorRunner/StorageProxy.cs
+++ DIDAOperatorSample/OperatorRunner/StorageProxy.cs
@@ -16,7 +16,9 @@ namespace Worker
     public class StorageProxy : IDIDAStorage
     {
         // dictionary with storage gRPC client objects for all storage nodes DIDAStorageService.DIDAStorageServiceClient
-        Dictionary<string, DIDAStorageService.DIDAStorageServiceClient> _clients = new Dictionary<string, DIDAStorageService.DIDAStorageServiceClient>();
+        //Dictionary<string, DIDAStorageService.DIDAStorageServiceClient> _clients = new Dictionary<string, DIDAStorageService.DIDAStorageServiceClient>();
+
+        Dictionary<int, Client> _clients = new Dictionary<int, Client>();
 
         // dictionary with storage gRPC channel objects for all storage nodes
         //Dictionary<string, GrpcChannel> _channels = new Dictionary<string, GrpcChannel>();
@@ -31,8 +33,9 @@ namespace Worker
             foreach (DIDAStorageNode n in storageNodes)
             {
                 //_channels[n.serverId] = GrpcChannel.ForAddress("http://" + n.host + ":" + n.port);
+                //_clients[n.serverId] = new DIDAStorageService.DIDAStorageServiceClient(channel);
                 GrpcChannel channel = GrpcChannel.ForAddress("http://" + n.host + ":" + n.port);
-                _clients[n.serverId] = new DIDAStorageService.DIDAStorageServiceClient(channel);
+                _clients[n.serverId.GetHashCode()] = new Client { id = n.serverId, client = new DIDAStorageService.DIDAStorageServiceClient(channel) };
             }
             _meta = metaRecord;
         }
@@ -42,31 +45,41 @@ namespace Worker
         // THE FOLLOWING 3 METHODS ARE THE ESSENCE OF A STORAGE PROXY
         // IN THIS EXAMPLE THEY ARE JUST CALLING THE STORAGE 
         // IN THE COMLPETE IMPLEMENTATION THEY NEED TO:
-        // 1) LOCATE THE RIGHT STORAGE SERVER -> hash
+        // 1) LOCATE THE RIGHT STORAGE SERVER
         // 2) DEAL WITH FAILED STORAGE SERVERS
-        // 3) CHECK IN THE METARECORD WHICH ARE THE PREVIOUSLY READ VERSIONS OF DATA 
-        // 4) RECORD ACCESSED DATA INTO THE METARECORD
+        // 3) CHECK IN THE METARECORD WHICH ARE THE PREVIOUSLY READ VERSIONS OF DATA ???
+        // 4) RECORD ACCESSED DATA INTO THE METARECORD ???
 
-        private DIDAStorageService.DIDAStorageServiceClient locateStorage(string id)
-        { // 1 and 2 
-            return _clients["s1"];
+        private DIDAStorageService.DIDAStorageServiceClient LocateStorage(string id)
+        { // (2)!! 
+            int hash = id.GetHashCode();
+            int closer = 0;
+            foreach(int i in _clients.Keys)
+            {
+                if (Math.Abs(hash - i) < Math.Abs(hash - closer))
+                {
+                    closer = i;
+
+                }
+            }
+            return _clients[closer].client;
         }
 
         public virtual DIDAWorker.DIDARecordReply read(DIDAWorker.DIDAReadRequest r)
         {
-            var res = _clients["s1"].read(new DIDAReadRequest { Id = r.Id, Version = new DIDAVersion { VersionNumber = r.Version.VersionNumber, ReplicaId = r.Version.ReplicaId } });
+            var res = LocateStorage(r.Id).read(new DIDAReadRequest { Id = r.Id, Version = new DIDAVersion { VersionNumber = r.Version.VersionNumber, ReplicaId = r.Version.ReplicaId } });
             return new DIDAWorker.DIDARecordReply { Id = res.Id, Val = res.Val, Version = { VersionNumber = res.Version.VersionNumber, ReplicaId = res.Version.ReplicaId } };
         }
 
         public virtual DIDAWorker.DIDAVersion write(DIDAWorker.DIDAWriteRequest r)
         {
-            var res = _clients["s1"].write(new DIDAWriteRequest { Id = r.Id, Val = r.Val });
+            var res = LocateStorage(r.Id).write(new DIDAWriteRequest { Id = r.Id, Val = r.Val });
             return new DIDAWorker.DIDAVersion { VersionNumber = res.VersionNumber, ReplicaId = res.ReplicaId };
         }
 
         public virtual DIDAWorker.DIDAVersion updateIfValueIs(DIDAWorker.DIDAUpdateIfRequest r)
         {
-            var res = _clients["s1"].updateIfValueIs(new DIDAUpdateIfRequest { Id = r.Id, Newvalue = r.Newvalue, Oldvalue = r.Oldvalue });
+            var res = LocateStorage(r.Id).updateIfValueIs(new DIDAUpdateIfRequest { Id = r.Id, Newvalue = r.Newvalue, Oldvalue = r.Oldvalue });
             return new DIDAWorker.DIDAVersion { VersionNumber = res.VersionNumber, ReplicaId = res.ReplicaId };
         }
     }
